9b0f58d5416ec8d174d0a5a61837d9ebd3b33b1c
Jelmer Wessels <jelmerw@live.nl>
2018-05-14 11:23:03 +0200

    added friends

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
-import logo from './logo.svg';
-import Icon from './components/components.js'
+import {Icon, Friends} from './components/components.js'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+            <div>
                 <table width="100%">
                     <tbody>
                     <tr>
-                        <td align="center"><Icon/></td>
+                        <td align="center"><Icon online={true}/></td>
                         <td align="center"><Icon action={() => alert('This value has been changed.')}/></td>
-                        <td align="center"><Icon image='https://i.redd.it/g5fz99po32u01.png'
-                                                 action={() => alert('THIS ... IS ... SPARTA !!!.')}/></td>
                     </tr>
                     </tbody>
                 </table>
+                <Friends friends={[' Dit is Piet', 'Henk', 'Klaas', 'Kees', 'Tom', 'Nick', 'Henk2', "Piet3"]}/>
             </div>
         );
     }
--- frontend/src/components/components.js
+++ frontend/src/components/components.js
@@ -8,6 +8,7 @@ class Icon extends React.Component {
         this.state = {
             image: props.image,
             action: props.action,
+            online: props.online,
         };
     }
 
@@ -18,13 +19,59 @@ class Icon extends React.Component {
         if (this.state.action === undefined) {
             this.setState({action: () => alert("No action set.")});
         }
+        if (this.state.online === undefined) {
+            this.setState({online: false});
+        }
     }
 
     render() {
+        let color = '#343434';
+        if(this.state.online){
+            color = '#8F6593';
+        }
         return (
-            <img className='icon' src={this.state.image} onClick={this.state.action} alt=''/>
+            <img className='icon' src={this.state.image} onClick={this.state.action} alt='' style={{'border-color':color}}/>
         )
     }
 }
 
-export default Icon;
+class Friends extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            friends: props.friends,
+        }
+    }
+
+    componentDidMount() {
+        if (this.state.friends === undefined) {
+            this.setState({friends: ['no friends']});
+        }
+    }
+
+    render() {
+        let list = [];
+        if (this.state.friends !== undefined) {
+            for (let i = 0; i < this.state.friends.length; i++) {
+                let info = this.getInfo(this.state.friends[i]);
+                list.push(<td key={i}><Icon action={() => alert(info.name)} online={info.online} image={info.image}/></td>);
+            }
+        }
+        return (
+            <div className='friendsList'>
+                <h3>Active Friends</h3>
+                <table>
+                    <tbody>
+                    <tr>{list}</tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+    getInfo(friend){
+        return {name:friend, online:false, image:undefined};
+    }
+}
+
+export {Icon, Friends};
