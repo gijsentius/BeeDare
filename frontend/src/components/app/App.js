f1460f7750e05fdbc95c88bf7320506ec26a943c
entix <g.m.entius@gmail.com>
2018-05-15 20:31:32 +0200

    rage quit css

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,25 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import LoginRegisterPage from '../user_interaction/login-registerpage';
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
-				<nav>
-					<div class="nav-wrapper amber darken-1">
-						<Link to="/"><a href="#" class="brand-logo">Logo</a></Link>
-						<ul id="nav-mobile" class="right hide-on-med-and-down">
-							<li><Link to="/">Sass</Link></li>
-							<li><Link to="/">Components</Link></li>
-							<li><Link to="/">JavaScript</Link></li>
-						</ul>
+				<div>
+					<nav>
+						<div className="nav-wrapper amber darken-1">
+							<Link to="/"><a className="brand-logo">Logo</a></Link>
+							<ul id="nav-mobile" className="right hide-on-med-and-down">
+								<li><Link to="/">Sass</Link></li>
+								<li><Link to="/">Components</Link></li>
+								<li><Link to="/">JavaScript</Link></li>
+							</ul>
+						</div>
+					</nav>	
+					<div className="content">
+							<Route exact path="/" component={LoginRegisterPage}/>
 					</div>
-				</nav>
-
-				{/* <Route exact path="/" component={Home} />
-      	<Route path="/about" component={About} />
-      	<Route path="/topics" component={Topics} /> */}
+				</div>
 			</Router>
 		);
 	}
