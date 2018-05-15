c98e490867b0e9125a81aad9c20fa25035dabc0a
Jelmer Wessels <jelmerw@live.nl>
2018-05-15 14:22:43 +0200

    improved colors

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import Friends from "../friends/Friends";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Friends friends={["Piet", "Klaas", "Gijs"]}/>
+
             </div>
         );
     }
