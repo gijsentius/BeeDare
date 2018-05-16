b7f60d1e45e7dc1aa3f8190c1b1520500d65d192
steve <sp.hilberts@outlook.com>
2018-05-16 17:22:42 +0200

    Added friendslist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App.js'
+import Friendslist from "./components/friends/Friendslist";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Friendslist />, document.getElementById('root'));
+
+//ReactDOM.render(<App />
