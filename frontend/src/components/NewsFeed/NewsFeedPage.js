abc8dbaff3222bddfec3dae286b9b1a8b3edb3d8
Daggh <d.g.hoogendorp@st.hanze.nl>
2018-05-17 14:18:25 +0200

    Openchallenges and start profilepage

--- frontend/src/components/NewsFeed/NewsFeedPage.js
+++ frontend/src/components/NewsFeed/NewsFeedPage.js
@@ -24,17 +24,19 @@ class NewsFeedPage extends React.Component {
         const profileInfo = this.state.profileInfo;
 
         return (
-            <div>
+            <div className="customContainer">
                 <div className="row">
-                    <div className="col s12 m3 sticky">
-                        <Profile profileInfo={profileInfo}/>
-                    </div>
-                    <div className="col s12 m6">
+                        <div className="col s12 m3 sticky">
+                            <Profile profileInfo={profileInfo}/>
+                        </div>
+
+                    <div className="col s12 m6 droppedShadowBoxNoScroll">
                         <Newsfeed/>
                     </div>
-                    <div className="col s12 m3 sticky">
-                        <Hives className="col s4" hives={[<Hive/>, <Hive/>, <Hive/>]}/>
-                    </div>
+                        <div className="col s12 m3 sticky">
+                            <Hives className="col s4" hives={[<Hive/>, <Hive/>, <Hive/>]}/>
+                        </div>
+
                 </div>
             </div>
         )
