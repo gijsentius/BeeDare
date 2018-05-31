922979c559a52f7d82201334eeb66cd471b133c0
Daggh <d.g.hoogendorp@st.hanze.nl>
2018-05-31 17:08:43 +0200

    newest additions to the frontend landingpage

--- frontend/src/components/landingPage/LandingPage.js
+++ frontend/src/components/landingPage/LandingPage.js
@@ -1,16 +1,68 @@
 import React from 'react';
 import Register from "../user_interaction/register";
 import Landinginfo from "./Landinginfo";
+import backgroundImage from "../../images/Background.jpg"
+import placeHolder from "../../images/placeholder.jpg"
 
 class LandingPage extends React.Component{
     render(){
         return(
-            <div className="row pageSection">
-                <div className="col s12"><span
-                    className="flow-text">I am always full-width (col s12)</span></div>
-                {/*<div className="col m3 offset-m4" id="register">*/}
-                    {/*<Register />*/}
-                {/*</div>*/}
+            <div className="marginRemover">
+                <div className="pageSection" style={{backgroundColor: "#fff8e1"}}>
+                </div>
+                <div className="pageSection" >
+
+                </div>
+                <div className="pageSection" style={{backgroundImage: `url(${backgroundImage})`}} >
+                    {/*style={{backgroundColor: "#e3f2fd"}}*/}
+                    <div className="row centerContent">
+
+                        <div className="col s4">
+                            <div className="card">
+                            <div className="card-content">
+                                <div className="card-image">
+                                    <img src={placeHolder}/>
+                                        <span className="card-title">Card Title</span>
+                                </div>
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                    I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col s4">
+                            <div className="card">
+                                <div className="card-content">
+                                    <div className="card-image">
+                                        <img src={placeHolder}/>
+                                        <span className="card-title">Card Title</span>
+                                    </div>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                        I am convenient because I require little markup to use effectively.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col s4">
+                            <div className="card">
+                                <div className="card-content">
+                                    <div className="card-image">
+                                        <img src={placeHolder}/>
+                                        <span className="card-title">Card Title</span>
+                                    </div>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                        I am convenient because I require little markup to use effectively.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="pageSection" style={{backgroundColor: "#40F7FF"}}>
+
+                </div>
+                <div className="col m1 floatingRegister">
+                    <Register />
+                </div>
+
             </div>
         );
     }
