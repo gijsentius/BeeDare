d0e83a8d5a08c273197a82b5306cc2dde3b9296c
Daggh <d.g.hoogendorp@st.hanze.nl>
2018-06-03 15:31:40 +0200

    new images, new info on landingpage

--- frontend/src/components/landingPage/CardRow.js
+++ frontend/src/components/landingPage/CardRow.js
@@ -1,44 +1,59 @@
 import React from 'react';
-import placeHolder from "../../images/placeholder.jpg"
+import friendBees from "../../images/friendBees.jpg"
+import honey from "../../images/honey.jpg"
+import beehives from "../../images/beehives.jpg"
 
 class CardRow extends React.Component{
     render(){
         return(
-              <div className="row centerContent">
+              <div className="row valign-wrapper">
 
-                <div className="col s4">
+                <div className="col s3">
                     <div className="card">
                         <div className="card-content">
                             <div className="card-image">
-                                <img src={placeHolder}/>
-                                <span className="card-title">Card Title</span>
+                                <img src={friendBees}/>
+                                <span className="card-title">Follow your friends</span>
                             </div>
-                            <p>I am a very simple card. I am good at containing small bits of information.
-                                I am convenient because I require little markup to use effectively.</p>
+                            <p><br/>
+                                Follow your friends and see how good your friends are doing!
+                                Do they have more honey than you?
+                                What kind of hives do they follow?
+                                See which Dares they accepted and completed!
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="col s4">
+
+                  <div className="col s3">
+                      <div className="card">
+                          <div className="card-content">
+                              <div className="card-image">
+                                  <img src={beehives}/>
+                                  <span className="card-title">Become a hive member</span>
+                              </div>
+                              <p><br/>
+                                  Become a member of as many hives you want!
+                                  Make new Dares together with your colony.
+                                  Start your own hive and become the queen of the hive.</p>
+                          </div>
+                      </div>
+                  </div>
+
+                <div className="col s3">
                     <div className="card">
                         <div className="card-content">
                             <div className="card-image">
-                                <img src={placeHolder}/>
-                                <span className="card-title">Card Title</span>
+                                <img src={honey}/>
+                                <span className="card-title">Complete Dares and earn honey</span>
                             </div>
-                            <p>I am a very simple card. I am good at containing small bits of information.
-                                I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col s4">
-                    <div className="card">
-                        <div className="card-content">
-                            <div className="card-image">
-                                <img src={placeHolder}/>
-                                <span className="card-title">Card Title</span>
-                            </div>
-                            <p>I am a very simple card. I am good at containing small bits of information.
-                                I am convenient because I require little markup to use effectively.</p>
+                            <p>
+                                <br/>
+                                Complete as many Dares as you can and earn gallons of honey!
+                                Maybe even earn Royal Jelly ad some point.
+                                By completing Dares you earn honey points.
+                                The more points you have, the higher up the bee rank you go!
+                            </p>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/user_interaction/register_page.js
+++ frontend/src/components/user_interaction/register_page.js
@@ -7,9 +7,12 @@ export default class LoginRegisterPage extends React.Component{
 
     render(){
         return(
+            <div className="row">
                 <div className="col m6 s12 offset-m3" id="register">
                     <Register />
                 </div>
+            </div>
+
         );
     }
 }
