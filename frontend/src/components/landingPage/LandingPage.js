b990d0f20c464786136ded77f8ad18c9c61b7cb2
Daggh <d.g.hoogendorp@st.hanze.nl>
2018-06-03 16:48:02 +0200

    added lots of style thingies

--- frontend/src/components/landingPage/LandingPage.js
+++ frontend/src/components/landingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Register from "../user_interaction/register";
+import './LandingPage.css';
 import backgroundImage from "../../images/Background.jpg"
 import beedareLaptop from "../../images/beedare_on_laptop.png"
 import scrollToComponent from 'react-scroll-to-component';
@@ -8,14 +9,14 @@ import CardRow from "./CardRow";
 class LandingPage extends React.Component{
     render(){
         return(
-            <div className="main">
+            <div className="main z-depth-1">
 
-                <div className="pageSection" ref={(section) => { this.YellowBG = section; }} style={{backgroundColor: "#fff8e1"}}>
-                    <h3 className="textPositionLP"> Dare yourself to Bee <br/> more green! </h3>
+                <div className="pageSection z-depth-1" ref={(section) => { this.YellowBG = section; }} style={{backgroundColor: "#fff8e1"}}>
+                    <h4 id="textPositionLP" style={{color: "#255957", fontWeight:"bold"}}> Dare yourself to Bee <br/> more green! </h4>
+                    <img className="floatingLaptop" src={beedareLaptop}
+                         style={{maxWidth: "50vw", maxHeight: "auto"}}/>
+                    {/*Blijkbaar is het dus nodig om zo een afbeelding te resizen, in de css werkt niet lekker*/}
                     <div className="center-align">
-                        <img className="floatingLaptop" src={beedareLaptop}
-                        style={{maxWidth: "50vw", maxHeight: "auto"}}/>
-                        {/*Blijkbaar is het dus nodig om zo een afbeelding te resizen, in de css werkt niet lekker*/}
                         <button
                                 onClick={() => scrollToComponent(this.White, { offset: 0, align: 'top', duration: 1500})}
                                 className="center-bottom btn-floating btn-medium waves-effect amber accent-4"><i
@@ -23,7 +24,9 @@ class LandingPage extends React.Component{
                     </div>
                 </div>
 
-                <div className="pageSection" ref={(section) => { this.White = section; }}>
+                <div className="pageSection z-depth-1" ref={(section) => { this.White = section; }}>
+                    <blockquote id="textPositionQuote"><h4 style={{fontStyle:"italic", color: "#512da8"}}>
+                        "Every drop in the ocean counts"<br/>- Yoko Ono </h4></blockquote>
                     <div className="center-align">
                         <button
                             onClick={() => scrollToComponent(this.PictureBG, { offset: 0, align: 'top', duration: 1500})}
@@ -32,7 +35,7 @@ class LandingPage extends React.Component{
                     </div>
                 </div>
 
-                <div className="pageSection" ref={(section) => { this.PictureBG = section; }}
+                <div className="pageSection z-depth-1" ref={(section) => { this.PictureBG = section; }}
                      style={{backgroundImage: `url(${backgroundImage})`}}>
                     <CardRow/>
                     <div className="center-align">
@@ -43,7 +46,7 @@ class LandingPage extends React.Component{
                     </div>
                 </div>
 
-                <div className="pageSection" ref={(section) => { this.Blue = section; }} style={{backgroundColor: "#255957"}}>
+                <div className="pageSection z-depth-1" ref={(section) => { this.Blue = section; }} style={{backgroundColor: "#255957"}}>
                     <div className="center-align">
                         <button
                             onClick={() => scrollToComponent(this.Register, { offset: 0, align: 'top', duration: 1500})}
