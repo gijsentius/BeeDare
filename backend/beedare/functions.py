1f99926beae24b25c6991dd38522b0a221d2636b
Jelmer Wessels <jelmerw@live.nl>
2018-06-04 15:29:40 +0200

    views final fon now

--- backend/beedare/functions.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-from flask import jsonify
-
-
-def commit(session):
-    try:
-        session.commit()
-    except sqlalchemy.exc.IntegrityError:
-        return jsonify({"error": "commit failed"}), 401
